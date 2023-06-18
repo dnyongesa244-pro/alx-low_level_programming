eea412d122f257bc3d906de8faf43a648e11c989
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 03:27:51 -0700

    added dclaration

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * the main function is the entry point to the program
  */
 int main(void)
-
+}
 	printf("Size of a char: %zu byte(s)\n",sizeof(char));
 	printf("Size of an int: %zu byte(s)\n",sizeof(int));
 	printf("size of a long int: %zu byte(s)\n",sizeof(long int));
