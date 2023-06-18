fe4402ed5cac2b110adc7438ea221bc27c1d20fa
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 16:44:40 -0700

    a code to print lower alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+ *This is a c code to print lower alpha a-z skipping e and q
+ */
+int main(void)
+{
+	int i;
+	for (i = 97 ; i <= 122 ; i ++ )
+	{
+		if (i==101)
+		{
+			continue;
+		}
+		
+		if (i == 113)
+		{
+			continue;
+		}
+		
+		putchar (i);
+	}
+	putchar ('\n');
+    
+	return (0);
+
+}
