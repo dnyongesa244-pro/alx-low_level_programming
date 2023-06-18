c85627d7106cc2ffeb5d4d443f10e8ba02d6ecf7
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-17 21:52:27 -0700

    added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * this is a c program stating with a header file
+ * itn main is the main function 
+ * the program is a simple out put with puts format
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
