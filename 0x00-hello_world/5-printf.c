45b834a5d8cbb85dcee80ff9b986136cba52dae8
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 01:47:10 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * This is a c program that gives an outputh throughtrint method
+ *
+ * Return 0 means succes
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
