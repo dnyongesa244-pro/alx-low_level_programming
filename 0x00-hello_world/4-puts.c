e4246d4cef9ef7f5e03af5312bdac574d6751a88
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 01:30:39 -0700

    space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *main - Entry point of the program
  *
  *Description: C program that prints a message using the puts function.
- * 
+ *
  *Return: Always 0 (indicating successful execution)
  *
  */
