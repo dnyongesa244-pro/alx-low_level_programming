b5ef1fdd3c01945d3914abee4b82d64d1274f307
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 02:22:44 -0700

    size of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * This is a c program with a header
+ *
+ * the main function is the entry point to the program
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)",sizeof(char));
+	printf("Size of an int: %zu byte(s)",sizeof(int));
+	printf("size of a long int: %zu byte(s)",sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)",sizeof(long long int));
+	printf("Size of a float: %zu byte(s)",sizeof(float));
+}
