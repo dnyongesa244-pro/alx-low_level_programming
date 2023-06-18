fbbbd1c3d274e41455c008f52f1c1b1fdcf40107
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 00:35:57 -0700

    removed  spasces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
- #include <stdio.h>
+#include <stdio.h>
 
 /**
- * Thi is a c program starting with aheader
- *
- * it has the main function ich is the entry point to a program
- *
- * it give an output usinf=g puts methos
- */
+* Thi is a c program starting with aheader
+*
+* it has the main function ich is the entry point to a program
+*
+* it give an output usinf=g puts methos
+*/
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+        return 0;
 }
 
