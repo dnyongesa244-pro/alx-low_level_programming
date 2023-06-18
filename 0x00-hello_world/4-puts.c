75582237edb2c9e7330ea030d5719f1146aaf9ae
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 00:54:55 -0700

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-
-/*
-* Thi is a c program starting with aheader
-*
-* it has the main function ich is the entry point to a program
-*
-* it give an output usinf=g puts methos
-*/
+/**
+ *
+ * this is a c program starting with a c file
+ *
+ * it has an entrry point main
+ *
+ * print through put mentod
+ *
+ */
 int main(void)
 {
-    return puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
