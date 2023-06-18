9da3d2c4bc2b7e1f816277d08472f66783bbab6f
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 12:13:24 -0700

    message

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * This is a c program 
+ *
+ * it print s lowew and uppert alpha
+ * /
+int main()
+{
+	int i;
+	for(i = 97 ; i <=122 ; i++)
+	{
+		putchar(i);
+	}
+	for(i = 65 ; i <=90 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return 0;
+}
