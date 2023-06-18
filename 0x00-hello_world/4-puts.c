012dd8a76dea416d360f2775599fee89488ebddd
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 01:41:51 -0700

    comment om entry poitn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *
  *Return: Always 0 (Success)
  */
-int main(void)
+int main(void)/*This is the entry point of the program*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
