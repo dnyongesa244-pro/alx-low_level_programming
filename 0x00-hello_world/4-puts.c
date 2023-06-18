73dac3ea425b9e8e0a702570fc50d29844d7124e
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 01:28:34 -0700

    comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  *
  *main - Entry point of the program
  *
- *This is a C program that prints a message using the puts function.
- *
+ *Description: C program that prints a message using the puts function.
+ * 
  *Return: Always 0 (indicating successful execution)
  *
  */
