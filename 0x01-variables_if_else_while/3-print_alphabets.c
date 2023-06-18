eedbc2749e4099917fa2195a8b5fdc2c69e44c00
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 12:25:21 -0700

    message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /*
- * This is a c program 
+ *This is a c program 
  *
- * it print s lowew and uppert alpha
+*It print s lowew and uppert alpha
  *
  */
 int main()
 {
 	int i;
-	for(i = 97 ; i <=122 ; i++)
+	for(i = 97 ; i <= 122 ; i++)
 	{
 		putchar(i);
 	}
-	for(i = 65 ; i <=90 ; i++)
+	for(i = 65 ; i <= 90 ; i++)
 	{
 		putchar(i);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
