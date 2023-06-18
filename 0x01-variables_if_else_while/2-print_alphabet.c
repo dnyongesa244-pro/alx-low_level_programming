9fdb865a76d980bab13794faeb70bc52aa405062
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 11:55:35 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * this is a c file
+ *
+ *it prints alphabet a to z
+ */
+int main(void)
+{
+	for (int i = 97 ; i <= 122 ; i++ )
+	{
+		putchar( i );
+	}
+	putchar('\n');
+	return (0);
+}
