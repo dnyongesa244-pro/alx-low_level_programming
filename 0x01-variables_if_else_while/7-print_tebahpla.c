0f246fe4b2d09f28a4500e5426e9b589c303e23f
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 17:59:55 -0700

    printning lower alpha in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * this is a c code
+ *
+ * it print lower alpha in reverse order
+ */
+int main(void)
+{
+	int i;
+	
+	for (i =122 ; i >= 97 ; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return(0);
+}
