764a13d7ddf44408d1cbda38d1ba9d605b5d1202
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 17:43:48 -0700

    try

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,14 @@
 /*
  * this is a c program to print integer 1-9
  */
-int main(void)
-{
-        int i;
-        for (i = 0 ; i <= 9 ; i++)
-        {
-              	putchar(i+'0');
-	}
-	putchar('\n');
-	return (0);
-}
+ int main(void)
+ {
+ 	int i;
+ 	
+ 	for (i = 0 ; i <= 9 ; i++)
+ 	{
+ 		putchar(i+'0');
+	 }
+	 putchar('\n');
+	 return (0);
+ }
