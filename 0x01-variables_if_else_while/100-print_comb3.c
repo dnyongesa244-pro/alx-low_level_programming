24308fa443b2a5705223c342bd707e2f241433a6
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-19 07:46:48 -0700

    hello

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-	int i ,j;
+	int i,j;
 
-	for (i = 0; i <9 ; i++)
+	for (i = 0; i < 9 ; i++)
 	{
 		for (j = i ; j <10 ; j++)
 		{
-			if(i == j)
+			if (i == j)
 			{
 				continue;
 			}
 			putchar(i + '0');
 			putchar(j + '0');
-			if ( i <8 || j < 9)
+			if ( i < 8 || j < 9)
 			{
 				putchar(',');
 				putchar(' ');
