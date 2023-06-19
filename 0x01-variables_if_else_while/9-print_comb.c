d50881be90732604c03b8a24c52b0e6abde1fdff
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 18:52:18 -0700

    numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * his i a c rogram
+ *
+ * it prins integers 1 to 10 separeted by comma
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <10 ; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		puchar('\t');
+	}
+	putchar('\n');
+	return (0);
