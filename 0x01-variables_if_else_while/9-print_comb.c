f68b5d4a6f6eb138bca82c5294649e0bd9f7b47d
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-18 18:18:11 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ * this is a c code
+ * 
+ * it prints hexadecimal digits
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0 ; i <= 15 ; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0;);
+		}
+		else
+		{
+			putchar((i-10) + 'A');
+		}
+	}
+	putchar('\n');
+	return 0;
