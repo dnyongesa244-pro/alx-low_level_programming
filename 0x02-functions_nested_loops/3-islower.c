b4393e661837448f02e74dc08cf0c5aaa3c4d7c5
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-20 14:47:54 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  _islower - is a function to check the letter
+ *
+ * @c -  is the parrameter pased to the function
+ *  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
