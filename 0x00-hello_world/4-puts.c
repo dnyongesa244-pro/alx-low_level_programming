b217142a9f8835cea5dfe7d6e8192bfc67e31a72
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-19 19:14:08 -0700

    mr=essah=ge

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- *Main - Entry point
+/**
+ * main - Entry point
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(void)/*This is the entry point of the program*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
