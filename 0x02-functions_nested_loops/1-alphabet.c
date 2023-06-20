859f46ad90b236ad528810f9b778d6b3d1ce012e
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-20 07:28:03 -0700

    push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_alphabet - prototype of void
+ */
+print_alphabet(void)
+{
+	int i;
+	for (i = 65 ; i <= 90 ; i++)
+	{
+		putchar(i);
+	}
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet(void);
+	return (0);
+}
