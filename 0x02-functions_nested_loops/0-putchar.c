de426fa7485c047cbf0687d98da2fefff407e30c
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-20 06:19:45 -0700

    hello

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h";
+/**
+ * main - enry point to the program
+ *
+ * Return: 0 Always ((Success)
+ *
+ */
+void msg(const char* lett)
+{
+	int i=0;
+	while(lett[i]!=('\0'))
+	{
+		_putchar(lett[i]);
+		i++;
+	}
+}
+int main(void)
+{
+	const char* word = "_putchar";
+	msg(word);
+}
