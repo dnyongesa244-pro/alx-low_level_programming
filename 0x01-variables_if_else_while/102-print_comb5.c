0693a926e9f1b01a1e0bc78b112bb37477b5528f
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-19 17:18:18 -0700

    removed white spaces

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /*
- *This is a C program that prints all possible combinations of two two-digit numbers.
+ *This is a C program that prints combination didit numbers.
  */
 int main(void)
 {
 	int i, j;
-	
+
 	for (i = 0 ; i <= 99 ; i++)
 	{
 		for (j = i ; j <= 99 ; j++)
@@ -14,13 +14,12 @@ int main(void)
 			if (i == j)
 			{
 				continue;
-			}
-			
-			putchar(i/10 + '0');
-			putchar(i%10 + '0');
+			}		
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
 			putchar(' ');
-			putchar(j/10 + '0');
-			putchar(j%10 + '0');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 			
 			if (i != 98 || j != 99)
 			{
