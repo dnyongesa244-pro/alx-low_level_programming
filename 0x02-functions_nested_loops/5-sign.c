82a27a82bc54436f89fb7ae373f7a9f8b33f9a2f
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-20 18:49:52 -0700

    messge

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,80 +1,28 @@
 #include "main.h"
-
-/*
- * printint - Is the function name
- *
- * @num: is the paramenter
- */
-
-void printint(int num)
-{
-    if (num < 0)
-    {
-        _putchar('-');
-        num = -num;
-    }
-
-    if (num == 0)
-    {
-        _putchar('0');
-    }
-    else
-    {
-        int reversenum = 0;
-        while (num != 0)
-        {
-            reversenum = reversenum * 10 + num % 10;
-            num /= 10;
-        }
-
-        while (reversenum != 0)
-        {
-            int digit = reversenum % 10;
-            _putchar(digit + '0');
-            reversenum /= 10;
-        }
-    }
-}
-
 /**
- * msg - is the function name
+ * print_sign - function nsme
  *
- * @ch: is the parameter
- */
-/*
-void msg(const char *ch)
-{
-    int i = 0;
-    while (ch[i] != '\0')
-    {
-        _putchar(ch[i]);
-        i++;
-    }
-}
-*/
-/**
- * num - is the funtion nme
+ * @n: parameter to pass
  *
- * @n: is the parameter
- *
- * Return: -1 (negative), 0 (zero), 1 (Positive)
+ * Return: 0 (zero), 1 (positive), -1 (Negative)
  */
 int print_sign(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        printint(-n);
-        return -1;
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return 0;
-    }
-    else
-    {
-        printint(n);
-        return 1;
-    }
+	if (n = 0)
+	{
+		_putchar(0);
+		return 0;
+	}
+	else if ( n < 0)
+	{
+		_putchar('-');
+		_putchar('1');
+		return (-1);
+	}
+	else
+	{
+		_putchar('+');
+		_putchar('1');
+		return (1);
+	}
 }
