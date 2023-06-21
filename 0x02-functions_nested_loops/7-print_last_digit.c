ec750f5b26bf137c81d19daf2dea9d07b1dfe982
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-21 03:31:23 -0700

    message

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,16 @@
  */
 int print_last_digit(int num)
 {
-	int x = num % 10;
-
+	int x;
+	if(num<0)
+	{
+		num *= -1;
+		x = num%10; 
+	}
+	else
+	{
+		x = num % 10;
+	}
 	_putchar(x + '0');
 	return (x);
 }
