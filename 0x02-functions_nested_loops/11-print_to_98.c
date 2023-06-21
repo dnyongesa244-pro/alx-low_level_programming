5a0ce638f07f0ef7e52b1263115b28592fd7bdab
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-21 16:53:50 -0700

    hello

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_to_98 - main function
+ */
+void print_to_98(int n)
+{
+	int i;
+	if (n <= 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+	    {
+	    	if (i != 98)
+	    	{
+	    		printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+	    }
+	}
+	if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+	}
+}
