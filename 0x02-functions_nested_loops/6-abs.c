433109998f9de0524c73bf94d6089e1aab6be995
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-21 02:29:49 -0700

    message

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - functiond name
+ *
+ * @value - parameter
+ */
+int _abs(int value)
+{
+	if (value <= 0)
+	{
+		int val = value * -1;
+		return val;
+	}
+	else
+	{
+		int val = value;
+		return val;
+	}
+}
