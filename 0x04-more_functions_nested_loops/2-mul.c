83197ed61d8f5c840e37ed0c02dbe2711c486c2c
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-22 18:46:54 +0300

    hello

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - funtion to add
+ *
+ * @a: first parameter
+ *
+ * @b: second parameter
+ *
+ * Return: rs Always (Success)
+ */
+int mul(int a, int b)
+{
+	int rs = a*b;
+	return rs;
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,8 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
+int mul(int a, int b);
+
 int largest_number(int a, int b, int c);
 
 void print_remaining_days(int month, int day, int year);
