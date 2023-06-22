8bf8539b8aa1550d2cc7b1a018f048b7d97c65b9
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-21 17:33:01 -0700

    hello

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 /**
  * add - the function to perform computation
+ *
+ * @a: the first parameter
+ *
+ * @b: the seconf=d parameter
+ *
+ * Return: sum Always (Succes)
  */
 int add(int a, int b)
 {
