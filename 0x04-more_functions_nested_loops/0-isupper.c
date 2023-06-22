40bfe92dfb300d93811d89ad6cba84f79a976f5a
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-22 06:53:22 -0700

    hello

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - function name
+ *
+ * @c: parameter
+ *
+ * Return: 1 (upper), 0(ohert")
+ *
+ */
+int _isupper(int c)
+{
+        if ((c >= 65) && (c <= 90))
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
