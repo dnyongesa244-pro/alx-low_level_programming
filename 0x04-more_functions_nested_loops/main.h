5a4c43910eafb41e1ea4b00a9b0f36a1e6129c21
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-22 07:38:20 -0700

    hello

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int _isupper(int c);
 
 int largest_number(int a, int b, int c);
 
-void print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year);
 
 void print_alphabet(void);
 
