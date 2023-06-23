4f4e9c1f96d885dd75cc4e1a9d946d72f1b6b60e
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-23 03:36:59 +0300

    hello

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * more_numbers - function name
  */
@@ -14,17 +14,17 @@ void more_numbers(void)
 		{
 			if (j < 10)
 			{
-				putchar(j+'0');
+				_putchar(j+'0');
 			}
 			else
 			{
-				putchar((j / 10)+'0');
-				putchar((j % 10)+'0');
+				_putchar((j / 10)+'0');
+				_putchar((j % 10)+'0');
 			}
 			j++;
 		}
-		putchar('\n');
+		_putchar('\n');
 		count++;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
