0eddade2882d1e6d005b2f15c9b84dcee23fd26a
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-23 17:14:01 +0300

    hello

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * prime - the function to compute prime factors
+ *
+ * @number: the number whose prime factor is to be computed
+ */
+void prime(long number)
+{
+	long i;
+	int x;
+	for (i = 2 ; i <= number ; i++)
+	{
+		while (number % i == 0)
+		{
+			number /= i;
+			x = (long)i;
+		}
+	}
+	printf("%d", x);
+	printf("\n");
+}
+int main()
+{
+	prime(612852475143);
+	return (0);
+}
