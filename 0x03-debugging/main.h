c4a242994d99b7b795f25f06619dde97be217d52
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-26 07:46:53 +0300

    hello

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,7 +8,9 @@ int largest_number(int a, int b, int c);
 
 void positive_or_negative(int i);
 
-/*void print_remaining_days(int month, int day, int year)*/
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
 
 void print_alphabet(void);
 
