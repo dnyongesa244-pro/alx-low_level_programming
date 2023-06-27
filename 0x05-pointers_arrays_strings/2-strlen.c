40a0e764ed1241e81024c94011758040b1e183f8
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-27 03:17:36 +0300

    hello

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Function to compute the lenht of the string
+ *
+ * @s: parameter to pass the string
+ *
+ * Return: 0 Always (Success)
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while(s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
