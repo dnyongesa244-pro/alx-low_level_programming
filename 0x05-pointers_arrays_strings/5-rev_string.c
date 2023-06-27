2d2a1dc59ad745fca988384699509915b51d6e40
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-27 12:22:37 +0300

    swap

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - function to reverse string
+ *
+ * @s: the parameter to pas the string
+ */
+
+void rev_string(char *s)
+{
+	int x = strlen(s);
+	int i, j, temp;
+
+	for(i = 0 , i = x - 1 ; i < j ; i++ , j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
