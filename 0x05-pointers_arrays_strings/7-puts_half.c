52267ac5143882f5645eb5ae2d1377bf5773dc35
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-27 16:03:57 +0300

    hello

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,14 @@ void puts_half(char *str)
 	int length = strlen(str);
 
 	int i;
-	int x = length/2;
+	int x = length / 2;
 
 	for (i = 0; i < length ; i++)
 	{
-		if (i > x)
+		if ((i+1) > x)
 		{
 			_putchar(str[i]);
 		}
 	}
+	_putchar('\n');
 }
