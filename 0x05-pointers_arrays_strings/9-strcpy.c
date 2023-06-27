2367445e642ed7784df11e005e6c47156c24733e
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-27 21:58:27 +0300

    hello

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - function to copy string
+ *
+ * @dest: destination address
+ *
+ * @src: source address
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest,src);
+	return dest;
+}
