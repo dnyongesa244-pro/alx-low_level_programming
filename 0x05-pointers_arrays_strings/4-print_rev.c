c178807dea2111ff0dfd9d13fd9f28a900fa516c
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-27 04:24:17 +0300

    hello

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - funtion to reverse
+ *
+ * @s: parsmeter to be reversed
+ */
+void print_rev(char *s)
+{
+	int x = strlen(s);
+
+	int i;
+
+	for(i=x;i>=0;i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
