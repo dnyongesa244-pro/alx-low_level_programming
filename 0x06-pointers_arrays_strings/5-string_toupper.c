2624e9e53fa5532f56661295451b6b99748b2ed7
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-06-30 01:10:42 +0300

    hello

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * string_toupper - function to convert to uppercase
+ *
+ * @lett: the parameter
+ *
+ * Return: lett Always (Success)
+ */
+
+char *string_toupper(char *lett)
+{
+	int i =0;
+	
+	while(lett[i]!=0)
+	{
+		lett[i] = toupper(lett[i]);
+		i++;
+	}
+	return (lett);
+}
+
