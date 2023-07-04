e0125fa6e6d8b0fb4d19270700c7264ae1f72723
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-04 14:02:16 +0300

    hello

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - function to compaire
+ *
+ * @s: First string
+ *
+ * @s: first string
+ *
+ * @accept: second string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] ; i++)
+	{
+		for (j = 0; accept[j] ; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (NULL);
+}
