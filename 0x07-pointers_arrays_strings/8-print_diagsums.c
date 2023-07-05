04a7da2ea154706aa3a37b538f53416e93c52bd1
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-05 03:48:57 +0300

    hello

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - function to print the sum
+ *
+ * @a: array integers
+ *
+ * @size: the size of the array
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i <= (size * size); i = i + size + 1)
+	{
+		sum1 = sum1 + a[i];
+	}
+	for (j = size - 1 ; j <= (size * size) - size ; j = j + size - 1)
+	{
+		sum2 = sum2 + a[j];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
