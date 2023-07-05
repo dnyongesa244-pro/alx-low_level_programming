65e683e5bfe1c5e0f16f99a8ca095de4fd811403
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-05 04:05:20 +0300

    hello

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function to set value to a pointer
+ *
+ * @s: first parameter
+ *
+ * @to: second paramerter
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
