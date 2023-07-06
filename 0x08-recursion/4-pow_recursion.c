44a4a5daffe4f28933f6985755e138d9d0fbf082
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-06 14:30:49 +0300

    power of a umber

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,14 +11,10 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int pow = 1;
-
+	if (y == 0)
+		return (1);
 	if (y < 0)
 		return (-1);
-	while (y > 0)
-	{
-		pow *= x;
-		y--;
-	}
-	return (pow);
+	else
+		return (x *  _pow_recursion(x, y - 1));
 }
