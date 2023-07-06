6e6a5f7c3fc3ae3f1b3a40d925ca86c5ce31d480
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-06 13:36:15 +0300

    made betty styled

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,11 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if(s[i] == '\0')
+	if (s[i] == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar(s[i]);
-	_puts_recursion(s+1);
+	_puts_recursion(s + 1);
 }
