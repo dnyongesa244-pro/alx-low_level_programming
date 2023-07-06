7c6de59a2ee25fc3f0d731d14e0d9b7e3ba47baa
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-07 00:43:22 +0300

    hello

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * me - function to check for prime number
+ *
+ * @x: number
+ *
+ * @y: checker
+ *
+ * Return: me(Success)
+ */
+int me(int x, int y)
+{
+	if (y == 1)
+	{
+		return (1);
+	}
+	if (x % y == 0)
+	{
+		return (0);
+	}
+	return (me(x, y - 1));
+}
+
+/**
+ * is_prime_number - function to check for one and egative
+ *
+ * @n: number
+ *
+ * Return: 1 (Success)
+ */
+int is_prime_number(int n)
+{
+	int c;
+
+	if (n < 0)
+		n = n * -1;
+	if (n == 1)
+		return (0);
+	c = n - 1;
+	return (me(n, c));
+}
