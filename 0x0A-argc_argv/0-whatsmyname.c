0275ea546d7a857fc50534a787b36db9319c50e9
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-08 05:31:39 +0300

    hello

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,19 +4,17 @@
 #include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - main function
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * @argc: The character to print
+ *
+ * @argv: stirng
+ *
+ * Return: On success 0.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
-int main(__attribute__((unused)) int argc,char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-        printf("%s\n",argv[0]);
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
