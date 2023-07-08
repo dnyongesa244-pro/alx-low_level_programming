afe59e0c5362b4a1b2180338cb640952cd8d3658
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-08 05:08:39 +0300

    argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,32 @@
 #include <string.h>
+#include <unistd.h>
 #include "main.h"
-int main(__attribute__((unused))  int argc, char argv[])
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	int i, x;
-	
-	char s[100];
+	return (write(1, &c, 1));
+}
 
-	x = strlen(argv[0]);
+int main(__attribute__((unused)) int argc,char *argv[])
+{
+        int i, x;
 
-	strcpy(s,argv[0]);
+        char s[100];
 
-	for( i = 0 ; i < x ; i++)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
-	return (0);
+        x = strlen(argv[0]);
+
+        strcpy(s,argv[0]);
+        for(i = 0; i < x ; i ++)
+        {
+                _putchar(s[i]);
+        }
+        _putchar('\n');
+        return 0;
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchat(char c);
+int _putchar(char c);
 
 #endif
