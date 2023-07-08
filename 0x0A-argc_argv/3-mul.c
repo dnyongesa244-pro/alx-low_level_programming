5bb89ed81660e06d79b9a0a53affe2fadcad96f9
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-08 15:56:02 +0300

    print arguments passed

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int i, x, y;
+
+	y = argc;
+
+	if (argc != 2)
+	{
+		print("Error");
+		return 1;
+	}
+	else
+	{
+		for( i = 0; i < ;y ; i ++;
+
+	}
+}
