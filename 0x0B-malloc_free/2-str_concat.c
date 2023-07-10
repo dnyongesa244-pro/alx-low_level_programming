85b72f3370aee9875d5e3cfa298174dce8b2bcd8
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-11 02:25:59 +0300

    hello

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,24 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int length1, length2;
+	int length1, length2, i;
 
 	char *string;
 
+	if(s1 == NULL && s2 != NULL)
+	{
+		i = strlen(s2);
+		string = malloc(sizeof(char) * i + 1);
+		strcpy(string, s2);
+		return (string);
+	}
+	if(s1 != NULL && s2 == NULL)
+	{
+		i = strlen(s1);
+		string = malloc(sizeof(char) * i +1);
+		strcpy(string, s1);
+		return (string);
+	}
 	if (s1 && s2 == NULL)
 		return (NULL);
 	length1 = strlen(s1);
