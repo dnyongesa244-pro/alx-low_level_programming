20d5eee934d98f9b979fc5e8032d13bf83892238
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-11 00:41:05 +0300

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - funtion to allocate string
+ *
+ * @str: string
+ *
+ * Return: array (Success)
+ */
+char *_strdup(char *str)
+{
+	int x = strlen(str);
+
+	int i;
+
+	char *array;
+
+	array = malloc(sizeof(char) * x);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0 ; i < x ; i++)
+	{
+		array[i] = str[i];
+	}
+	return (array);
+}
