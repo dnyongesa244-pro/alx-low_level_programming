8adb90483ff46101acc6a13b20a36cc6d2bec4bd
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-14 11:01:05 +0300

    hello

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,20 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * array_range - function t assing array range
+ *
+ * @min: starting ndex
+ *
+ * @max: ending index
+ *
+ * Return: numbers (SUccess)
+ */
 int *array_range(int min, int max)
 {
 	int *numbers;
+
 	int i, x;
 
 	if (min > max)
 		return (NULL);
 	x = max - min;
-	numbers = malloc(sizeof(int) * x +1);
-	if(numbers == NULL)
+	numbers = malloc(sizeof(int) * x + 1);
+	if (numbers == NULL)
 		return (NULL);
-	for(i=min;i<=max;i++)
+	for (i = min ; i <= max ; i++)
 	{
-		numbers[i] = min+i;
+		numbers[i] = min + i;
 	}
-	return numbers;
+	return (numbers);
 }
