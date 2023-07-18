550ee75b7f87555de16d9c766f5fd7eb97e63a80
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-18 05:20:43 +0300

    fixed bug

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,19 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO
 #define FUNCTION_LIKE_MACRO
 
-#define ADS(X);
-int ABS(int num)
-{
-	if(num == 0)
-	{
-		return 0;
-	}
-	if(num<0)
-	{
-		num *= -1;
-		return num;
-	}
-	else
-		return num;
-}
+#define ABS(x) ((x<0) ? -(x) : x)
+
 #endif
