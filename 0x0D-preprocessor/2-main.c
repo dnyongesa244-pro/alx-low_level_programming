3770eb7f3461d2f0ee7d80db0d7fef4a5aab7d79
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-18 05:32:29 +0300

    added betty style

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - main funtion
+ *
+ * Return: 0 (SUcess)
+ */
 int main(void)
 {
-	printf("%s\n",__FILE__);
-	return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
