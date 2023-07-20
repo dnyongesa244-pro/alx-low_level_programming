38434c6a358fa7a6bfd89203436c719bf97dcabe
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-20 18:40:43 +0300

    function to serch for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,17 +2,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i,j;
+	int i;
+	
 	if(size<0)
 		return -1;
 	for(i = 0; i < size ; i ++)
 	{
-		int (**P)(int) = &cmp;
-		for(j = 0 ; j < i ; i++)
-		{
-			if(array[i] == *p[j])
-				return i;
-		}
+		if(cmp(array[i]))
+			return i;
 	}
 	return -1;
 }
