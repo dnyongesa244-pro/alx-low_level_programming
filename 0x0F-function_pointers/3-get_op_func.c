e33aad3bd3ff58d50de2139da8d5f38a2a2dd6c6
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-21 16:20:06 +0300

    simple calclater

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,24 +1,32 @@
 #include "3-calc.h"
+#include <stdlib.h>
 
+/**
+ * get_op_func - function to choose the operator
+ *
+ * @s: operator
+ *
+ * Return: ops[i[.f (success)
+ */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
-
-    while (ops[i].op != NULL)
-    {
-        if (*ops[i].op == *s)
-        {
-            return ops[i].f;
-        }
-        i++;
-    }
-    return NULL;
+	int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*ops[i].op == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
