77c963da700ed5ca4c1fea4bcdd43b76b260eece
Daniel Nyongesa <dnyongesa244@gmail.com>
2023-07-22 00:31:19 +0300

    modified to betty coding style

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - main function
+ *
+ * @argc: first parameter
+ *
+ * @argv: second parameter
+ *
+ * Return: 0 Success
+ */
 int main(int argc, char *argv[])
 {
-	int i,nbytes;
-	if(argc != 2)
+	int i, nbytes;
+
+	if (argc != 2)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 
 	nbytes = atoi(argv[1]);
-	if(nbytes < 0)
+	if (nbytes < 0)
 	{
 		printf("Error\n");
-		return 2;
+		return (2);
 	}
-	char *mainptr = (char*) main;
+	char *mainptr = (char *) main;
 
-	for(i = 0 ; i < nbytes ; i++)
+	for (i = 0 ; i < nbytes ; i++)
 	{
 		printf("%02x ", mainptr[i] & 0xFF);
 	}
-	return 0;
+	return (0);
 }
