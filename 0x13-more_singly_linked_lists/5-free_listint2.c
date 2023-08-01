763192e39746ee25c44455d5eb38156fcdfd5db8
dnyongesa244-pro <dnyongesa244@gmail.com>
2023-08-02 02:30:34 +0300

    eddited the code to betty style

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,20 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_listint2 - function to free listint_t
+ *
+ * @head: head
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *current;
 	listint_t *next;
 
-	if(head == NULL)
+	if (head == NULL)
 		return;
 	current = *head;
-	while(current != NULL)
+	while (current != NULL)
 	{
 		next = current->next;
 		free(current);
