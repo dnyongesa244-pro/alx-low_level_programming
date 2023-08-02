8f7c4aaa6ba61270a47e0a20254a264b700873b6
dnyongesa244-pro <dnyongesa244@gmail.com>
2023-08-02 03:19:27 +0300

    modified to betty style

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,12 +12,13 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
+
 	listint_t *current = head;
 
-	while(current != NULL)
+	while (current != NULL)
 	{
-		if(count == index)
-			return current;
+		if (count == index)
+			return (current);
 		count++;
 		current = current->next;
 	}
