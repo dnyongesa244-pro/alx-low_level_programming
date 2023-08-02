6d2d14a6fc62bfde4887169e9c89e797b4b549d3
dnyongesa244-pro <dnyongesa244@gmail.com>
2023-08-02 03:44:21 +0300

    creatd function to insert new nose at given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function to insert node at index
+ *
+ * @head: head
+ *
+ * @idx: index
+ *
+ * @n: node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+	listint_t *current = *head;
+	listint_t *new_node;
+	if (idx == 0)
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	while (current != NULL && count < idx - 1)
+	{
+		current = current->next;
+		count++;
+	}
+	if (current == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
+}
