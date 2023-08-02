1640b10201e2e1606d23788202151c09a3eaf667
dnyongesa244-pro <dnyongesa244@gmail.com>
2023-08-02 04:12:43 +0300

    insert a node at a given posistion

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,4 +34,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
 #endif
